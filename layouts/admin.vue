8765eec72d46a453607a6740f96702d17ba76293
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-05-28 19:44:49 +0200

    modificato layout

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,6 +1,9 @@
 <template>
-  <h1>questo è il layout admin</h1>
-
+  <div
+    class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
+  >
+    <NuxtLink to="/">Template admin</NuxtLink>
+  </div>
   <!-- QUESTO E' UNO SPAZIO PER INSERIRE DATI  -->
   <div class="min-h-screen"><slot /></div>
   <!-- /QUESTO E' UNO SPAZIO PER INSERIRE DATI  -->
